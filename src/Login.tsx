2686d29292bd53f8513bdb76642edde195b5456c
elliehachemjob <elliehachemjob@gmail.com>
2021-11-29 10:27:24 +0200

    created authentication

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,18 +1,25 @@
 import React, { FC } from "react";
+import { Container } from "react-bootstrap";
 //space is %20
 //word wrap
 //alt z
 //?after endpoint for our params
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=&response_type=code&redirect_uri=http://localhost:3000/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+  "https://accounts.spotify.com/authorize?client_id=d85bfe356da5407ba7eab2e2c4504cf9&response_type=code&redirect_uri=http://localhost:3000/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 interface IProps {}
 
-/**
- * @author
- * @function @
- **/
-
 export const Login: FC<IProps> = (props) => {
-  return <div></div>;
+  return (
+    <div>
+      <Container
+        className="d-flex justify-content-center align-items-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <a className="btn btn-success btn-lg" href={AUTH_URL}>
+          Login with Spotify
+        </a>
+      </Container>
+    </div>
+  );
 };
